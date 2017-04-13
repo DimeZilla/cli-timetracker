635c746aa4d2238f68abd4b26254c5094a5458e5
Joshua Diamond <joshua@radcampaign.com>
2017-04-13 10:01:49 -0400

    adding completion message

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo 'Installing TimeLog! Get ready to be so productive!'
 DIR=$(pwd)
+
 #this will move our file into our bin
 cp $DIR/bin/timelog /usr/local/bin/
+
 #this will move our man script into our man1 folder
 if [ ! -d /usr/local/man ]; then
     mkdir /usr/local/man
@@ -10,4 +13,6 @@ fi
 if [ ! -d /usr/local/man/man1 ]; then
     mkdir /usr/local/man/man1
 fi
+
 cp $DIR/lib/timelog.1.gz /usr/local/man/man1
+echo 'All done. You are going to be so productive now!'
