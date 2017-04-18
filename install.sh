519b8a4bde03f8da59d2b6be2389a4c5454b4657
Joshua Diamond <joshua@radcampaign.com>
2017-04-18 02:25:48 -0400

    beginning to move program over to node

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo 'Installing TimeLog! Get ready to be so productive!'
 DIR=$(pwd)
 
 #this will move our file into our bin
-cp $DIR/bin/timelog /usr/local/bin/
+#cp $DIR/bin/timelog /usr/local/bin/
 
 #this will move our man script into our man1 folder
 if [ ! -d /usr/local/man ]; then
@@ -14,5 +14,6 @@ if [ ! -d /usr/local/man/man1 ]; then
     mkdir /usr/local/man/man1
 fi
 
-cp $DIR/lib/timelog.1.gz /usr/local/man/man1
+cp $DIR/assets/timelog.1.gz /usr/local/man/man1
+npm install -g
 echo 'All done. You are going to be so productive now!'
